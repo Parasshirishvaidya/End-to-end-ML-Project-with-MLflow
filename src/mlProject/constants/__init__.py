e4dec89b1cafc490c98dac581de8e5b28724d9fb
Paras Vaidya <paras.vaidya.official@gmail.com>
2025-01-23 18:35:52 +0530

    Data Ingestion Stage Done

---
+++ src/mlProject/constants/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("C:\Data Science Learning\Projects\End-to-end-ML-Project-with-MLflow\config\config.yaml")
+PARAMS_FILE_PATH = Path("C:\Data Science Learning\Projects\End-to-end-ML-Project-with-MLflow\params.yaml")
+SCHEMA_FILE_PATH = Path("C:\Data Science Learning\Projects\End-to-end-ML-Project-with-MLflow\schema.yaml")
+
+
--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -15,7 +15,7 @@ from typing import Any
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
-            content = yaml.safe_load()
+            content = yaml.safe_load(yaml_file)
             logger.info(f'yaml file: {path_to_yaml} loaded successfully')
             return ConfigBox(content)
     except BoxValueError:
